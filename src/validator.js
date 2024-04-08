9165674cae57cdd897e2e406dc6a4b532a457615
webdev.taras <webdev.taras@gmail.com>
2024-04-08 22:57:27 +0300

    feat: validator

--- src/validator.js
+++ src/validator.js
@@ -1,6 +1,26 @@
 module.exports = {
   rules: [],
-  validate() {},
-  assert() {},
-  custom() {},
+  validate(value) {
+    for(rule of this.rules) {
+      console.log('rule', rule)
+      if (rule(value) !== true)
+        return false
+    }
+    return true
+  },
+  assert(value) {
+    for(rule of this.rules) {
+      const result = rule(value)
+      if (result !== true) {
+        const message = (typeof result === 'string')
+          ? result
+          : 'validation error'
+        throw new TypeError(message)
+      }
+    }
+  },
+  custom(rule) {
+    this.rules.push(rule)
+    return this
+  },
 }
--- tests/validator.test.js
+++ tests/validator.test.js
@@ -12,3 +12,16 @@ test('validator interface', t => {
     'custom',
   ])
 })
+
+test('validator.validate()', t => {
+  validator
+    .custom(value => value != null)
+    .custom(value => typeof value === 'string')
+    .custom(value => value.length > 3)
+
+  ok(validator.validate('qwerty'))
+  ok(validator.validate('blablabla'))
+  ok(!validator.validate(null))
+  ok(!validator.validate(3))
+  ok(!validator.validate('no'))
+})
